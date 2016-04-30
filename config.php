925c90fa399bc592e14701f4b55f7b44b2c39f7b
home <1jingzhongren@163.com>
2016-04-30 19:09:54 +0800

    fixed

--- config.php
+++ config.php
@@ -6,23 +6,24 @@ define('HTTP_SERVER', 'http://huaye.app/');
 define('HTTPS_SERVER', 'http://huaye.app/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/xampp/htdocs/huaye/catalog/');
-define('DIR_SYSTEM', 'D:/xampp/htdocs/huaye/system/');
-define('DIR_IMAGE', 'D:/xampp/htdocs/huaye/image/');
-define('DIR_LANGUAGE', 'D:/xampp/htdocs/huaye/catalog/language/');
-define('DIR_TEMPLATE', 'D:/xampp/htdocs/huaye/catalog/view/theme/');
-define('DIR_CONFIG', 'D:/xampp/htdocs/huaye/system/config/');
-define('DIR_CACHE', 'D:/xampp/htdocs/huaye/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/xampp/htdocs/huaye/system/storage/download/');
-define('DIR_LOGS', 'D:/xampp/htdocs/huaye/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/xampp/htdocs/huaye/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/xampp/htdocs/huaye/system/storage/upload/');
+define('DIR_APPLICATION', '/usr/share/nginx/html/huaye/');
+define('DIR_SYSTEM', '/usr/share/nginx/html/huaye/system/');
+define('DIR_IMAGE', '/usr/share/nginx/html/huaye/image/');
+define('DIR_LANGUAGE', '/usr/share/nginx/html/huaye/language/');
+define('DIR_TEMPLATE', '/usr/share/nginx/html/huaye/view/template/');
+define('DIR_CONFIG', '/usr/share/nginx/html/huaye/system/config/');
+define('DIR_CACHE', '/usr/share/nginx/html/huaye/system/storage/cache/');
+define('DIR_DOWNLOAD', '/usr/share/nginx/html/huaye/system/storage/download/');
+define('DIR_LOGS', '/usr/share/nginx/html/huaye/system/storage/logs/');
+define('DIR_MODIFICATION', '/usr/share/nginx/html/huaye/system/storage/modification/');
+define('DIR_UPLOAD', '/usr/share/nginx/html/huaye/system/storage/upload/');
+define('DIR_CATALOG', '/usr/share/nginx/html/huaye/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'a723473');
 define('DB_DATABASE', 'huaye');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'mcc_');
