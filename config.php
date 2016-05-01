8fbaabfdfa4af56387b2fb8a4a1ef3de682443c9
qiyu <qiyu@huaye.com>
2016-05-01 11:22:09 +0800

    1

--- config.php
+++ config.php
@@ -20,9 +20,9 @@ define('DIR_UPLOAD', 'D:/xampp/htdocs/huaye/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_HOSTNAME', '101.200.151.83');
+define('DB_USERNAME', 'huaye');
+define('DB_PASSWORD', 'a7234738');
 define('DB_DATABASE', 'huaye');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'mcc_');
