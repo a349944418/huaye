e4d99edc0e65155adf8d20a17c07c1dedf846d55
home <1jingzhongren@163.com>
2016-06-05 14:36:17 +0800

    fixed

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
-// error_reporting(E_ERROR | E_WARNING | E_PARSE);  
-// ini_set('display_errors', 1);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);  
+ini_set('display_errors', 1);
 
 // Version
 define('VERSION', '1.4.0.0');
